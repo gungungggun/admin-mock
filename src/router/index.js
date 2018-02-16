eeeb695655e586389b9caf687cdbdd569f7fda9a
gun <gungungggun@gmail.com>
2018-02-16 18:58:42 +0900

    routerで認証

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Store from 'store'
 import Login from '@/pages/Login'
 import Top from '@/pages/Top'
 
+const jwt = Store.get('jwt')
+let auth = false
+if (jwt != null) {
+  auth = true
+}
+
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
@@ -16,7 +23,25 @@ export default new Router({
     {
       path: '/',
       name: 'Top',
-      component: Top
+      component: Top,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!auth) {
+      next({ path: '/login' })
+    }
+  } else {
+    if (auth) {
+      next({ path: '/' })
+    }
+  }
+  next()
+})
+
+export default router
