3d358aad653245fe0caba7aa96a3befb23c5f60b
gun <gungungggun@gmail.com>
2018-02-23 18:20:31 +0900

    layout

---
+++ src/components/layouts/BootstrapLayout.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  main(:id="prefix")
+    h1 {{ title }}
+    b-alert(show="") Default Alert
+
+    slot
+
+    div(@click="logout") ログアウト
+
+    global-side
+
+    global-footer
+</template>
+
+<script lang="coffee">
+import GlobalFooter from '@/components/globals/GlobalFooter'
+import GlobalSide from '@/components/globals/GlobalSide'
+
+export default
+  components:
+    GlobalFooter: GlobalFooter
+    GlobalSide: GlobalSide
+  props:
+    prefix:
+      default: ''
+      type: String
+    title:
+      default: ''
+      type: String
+</script>
+
+<style lang="stylus" scoped>
+h1, h2
+  font-weight normal
+  color #42b983
+</style>
--- src/pages/Top.vue
+++ src/pages/Top.vue
@@ -1,8 +1,5 @@
 <template lang="pug">
-  main#page-login
-    h1 トップ
-    b-alert(show="") Default Alert
-
+  bootstrap-layout(:prefix="'top'" :title="'Top'")
     b-container.bv-example-row
       b-row
         b-col 1 of 2
@@ -12,32 +9,17 @@
         b-col 2 of 3
         b-col 3 of 3
 
-    div(@click="logout") ログアウト
-
-    global-side
-
-    global-footer
 </template>
 
 <script lang="coffee">
-import GlobalFooter from '@/components/globals/GlobalFooter'
-import GlobalSide from '@/components/globals/GlobalSide'
+import BootstrapLayout from '@/components/layouts/BootstrapLayout'
 
 export default
   name: 'Top'
   metaInfo:
     title: 'Top'
   components:
-    GlobalFooter: GlobalFooter
-    GlobalSide: GlobalSide
-  data: () ->
-    form:
-      email: ''
-  methods:
-    logout: () ->
-      this.$store.dispatch 'logout'
-      .then (res) =>
-        this.$router.push '/login'
+    BootstrapLayout: BootstrapLayout
 </script>
 
 <style lang="stylus" scoped>
